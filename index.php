500418a2d7b082d571bc3c2d46ac5fbef152b4bc
ttaylorr <me@ttaylorr.com>
2014-03-30 16:32:14 -0400

    Add link to Github, copyright notice

--- index.php
+++ index.php
@@ -48,5 +48,11 @@
         $(".canvasjs-chart-credit").remove();
       </script>
     </div>
+    <footer style="padding-top: 48px;text-align:center;">
+      <div class="container text-muted">
+        Check us out on <a href="https://github.com/ttaylorr/mc">Github</a>!<br/>
+        &copy; <a href="http://ttaylorr.com">Taylor Blau</a> 2014, All Rights Reserved
+      </div>
+    </footer>
   </body>
 </html>
