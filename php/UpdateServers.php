a35e20c353cfff531e05332a02c2db4cae9df56b
ttaylorr <me@ttaylorr.com>
2014-03-30 11:07:29 -0400

    Only invoke UpdateServers over the commandline

--- php/UpdateServers.php
+++ php/UpdateServers.php
@@ -3,6 +3,10 @@
   require_once "DBPing.php";
 
   # This script should execute every minute and will ping all servers
+  if (php_sapi_name() != 'cli') {
+    die("This script may only be invoked over the command line!");
+  }
+
   $app = new Application("../config/config.json");
   $pinger = new MinecraftPing(30000);
 
