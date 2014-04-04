af1ef12e31ca3dfac076c3e5a29289b205394bb2
ttaylorr <me@ttaylorr.com>
2014-04-04 19:14:25 -0400

    Initial work on grunt integration

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+	grunt.initConfig({
+    cache: false,
+    mkdir: {
+      all: {
+        options: {
+          mode: 0700,
+          create: ['./tmp']
+        }
+      }
+    },
+    clean: {
+      short: ['tmp/']
+    },
+    phplint: {
+      options: {
+        swapPath: 'tmp/'
+      },
+      all: ['php/*.php']
+    },
+    uglify: {
+      build: {
+        src: ['js/app.js'],
+        dest: 'js/app.min.js'
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-mkdir');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-phplint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['mkdir:all', 'phplint:all', 'uglify', 'clean']);
+}
