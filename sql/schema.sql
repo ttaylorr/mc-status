0b47d73773a14016a9457ff8ea637a49cd969c20
Taylor Blau <me@ttaylorr.com>
2014-04-10 23:11:46 -0700

    Update sql

--- sql/schema.sql
+++ sql/schema.sql
@@ -9,9 +9,47 @@
 /*!40101 SET NAMES utf8 */;
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
--- Data exporting was unselected.
+
+-- Dumping database structure for mc-status
+CREATE DATABASE IF NOT EXISTS `mc-status` /*!40100 DEFAULT CHARACTER SET ascii */;
+USE `mc-status`;
+
+
+-- Dumping structure for table mc-status.pings
+CREATE TABLE IF NOT EXISTS `pings` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `server_name` varchar(64) NOT NULL DEFAULT '0',
+  `version` varchar(8) NOT NULL DEFAULT '0',
+  `players` mediumint(9) NOT NULL DEFAULT '0',
+  `maxplayers` mediumint(9) NOT NULL DEFAULT '0',
+  `ping` smallint(6) NOT NULL DEFAULT '0',
+  `time` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
 -- Data exporting was unselected.
+
+
+-- Dumping structure for table mc-status.servers
+CREATE TABLE IF NOT EXISTS `servers` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `ip` varchar(64) DEFAULT NULL,
+  `website` varchar(64) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii COMMENT='tracked servers';
+
+-- Data exporting was unselected.
+
+
+-- Dumping structure for table mc-status.services
+CREATE TABLE IF NOT EXISTS `services` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) DEFAULT NULL,
+  `status` varchar(16) DEFAULT NULL,
+  `time` bigint(20) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=ascii;
 
 -- Data exporting was unselected.
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
