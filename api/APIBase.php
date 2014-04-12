008a5e92642e30c0927da66b9d92ae63e512f5df
Taylor Blau <me@ttaylorr.com>
2014-04-11 17:10:42 -0700

    Cleanup

--- api/APIBase.php
+++ api/APIBase.php
@@ -2,12 +2,13 @@
   require_once './php/Application.php';
 
   abstract class APIBase {
-
     /**
      * Makes a call to whatever database the API needs to handle
      * @return The un-encoded form of the data returned.
      */
-    public static abstract function call();
+    public static function call() {
+      return array();
+    }
 
     /**
      * Handles and then outputs the call from `call()`, encoding
