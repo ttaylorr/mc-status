d404b306e6b8bae694a96152927a1a19be0570dd
Taylor Blau <me@ttaylorr.com>
2014-04-16 09:50:50 -0400

    Migrate static components of the page out and include them

--- index.php
+++ index.php
@@ -23,7 +23,6 @@
 
   $klein->dispatch();
 
-  include_once "php/AnalyticsTracking.php";
 ?>
 
 <!DOCTYPE html>
@@ -35,62 +34,40 @@
     <link href="css/min/bootstrap.min.css" rel="stylesheet">
     <link href="css/min/app.min.css" rel="stylesheet">
   </head>
+
   <body>
-    <header class="navbar navbar-default">
-      <div class="container">
-        <div class="navbar-header">
-          <a class="navbar-brand" href="#">Minecraft Server Tracking</a>
-        </div>
-        <div class="collapse navbar-collapse">
-          <ul class="nav navbar-nav navbar-right">
-            <li>
-              <a href="http://ttaylorr.com">Made with <span style="color:red;">&hearts;</span> by ttaylorr</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </header>
+    <?php include_once 'php/AnalyticsTracking.php'; ?>
+    <?php include 'static/Header.php'; ?>
     <div class="container">
       <h2>Tracked Servers <small>Updated every minute</small></h2>
+
       <?php
         echo($view->buildServerTables());
         $charter = new Charter($app);
       ?>
-      <div class="row row-fluid" id="mojang-status">
-        <div class="col-md-3">
-          <div class="alert alert-warning" data-service="minecraft.net" data-name='minecraft.net'></div>
-        </div>
-        <div class="col-md-3">
-          <div class="alert alert-warning" data-service="auth.mojang.com" data-name="Login Server"></div>
-        </div>
-        <div class="col-md-3">
-          <div class="alert alert-warning" data-service="sessionserver.mojang.com" data-name="Session Server"></div>
-        </div>
-        <div class="col-md-3">
-          <div class="alert alert-warning" data-service="skins.minecraft.net" data-name="Skin Server"></div>
-        </div>
-      </div>
+
+      <?php include 'static/Mojang.php'; ?>
+
       <ul class="nav nav-tabs">
         <li class="active"><a href="#" data-name="line">Line Graph</a></li>
         <li><a href="#" data-name="scatter">Scatter Chart</a></li>
         <li><a href="#" data-name="stackedArea">Stacked Area</a></li>
       </ul>
+
       <div id="chart" style="height: 525px; width: 100%;"></div>
 
       <script src="js/min/jquery-2.1.0.min.js"></script>
       <script src="js/min/bootstrap.min.js"></script>
       <script src="js/min/canvasjs.min.js"></script>
       <script src="js/min/app.min.js"></script>
+
       <script>
         renderPage(<?php echo $charter->render(); ?>);
       </script>
+
     </div>
-    <footer style="padding-top: 48px;text-align:center;">
-      <div class="container text-muted">
-        Check us out on <a href="https://github.com/ttaylorr/mc">Github</a>!<br/>
-        &copy; <a href="http://ttaylorr.com">Taylor Blau</a> 2014, All Rights Reserved<br/><br/>
-        <?php echo $app->rev(); ?>
-      </div>
-    </footer>
+
+    <?php include 'static/Footer.php'; ?>
+
   </body>
 </html>
---
+++ static/Footer.php
@@ -0,0 +1,7 @@
+<footer style="padding-top: 48px;text-align:center;">
+  <div class="container text-muted">
+    Check us out on <a href="https://github.com/ttaylorr/mc">Github</a>!<br/>
+    &copy; <a href="http://ttaylorr.com">Taylor Blau</a> 2014, All Rights Reserved<br/><br/>
+    <?php echo $app->rev(); ?>
+  </div>
+</footer>
