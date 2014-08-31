fa9313c311ff2ede63d5f05619d19668773459ea
Taylor Blau <me@ttaylorr.com>
2014-08-31 19:57:00 -0400

    Use redis/namespace

--- helpers/redis_helper.rb
+++ helpers/redis_helper.rb
@@ -1,10 +1,13 @@
 require 'redis'
+require 'redis-namespace'
 
 module MCStatus
   module Helpers
     module RedisHelper
+      # Public - returns the _namespaced_ Redis connection
       def redis
-        @redis ||= Redis.new(:host => 'localhost')
+        @redis_connection ||= Redis.new(:host => 'localhost')
+        @ns_redis ||= Redis::Namespace.new(:mc_status, :redis => @redis_connection)
       end
     end
   end
